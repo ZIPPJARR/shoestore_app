3ed1ea9fa63ef7ab71d989062fd4c21caffdfc90
avion <Avionk.smith@gmail.com>
2022-08-11 16:34:00 -0400

    started first portion of code cleaning.

--- shoestore.py
+++ shoestore.py
@@ -1,16 +1,8 @@
-#offline version of the api thats in github
-#nteractible script
 """requirement: should run forever untill user press quit
 the user should be able to purchase avavlible shoes from store
 the avalkible shoes should be pre defined by me.
 user should be able to sell their shoes on the store.
 user shopuld be able to see all of their shoes that they purchased."""
-
-
-
-
-
-
 
 print('Welcome To Shoestore!')
 print('Here you can purchase and sell any amount of shoes. ')
@@ -25,20 +17,17 @@ while True:
     print('to sell Shoes enter "Sell" ')
     print('to quit application enter "quit" ')
 
-   
     choice = input()
-    # user_cart['Nike']
     if choice == 'view':
         for i in user_cart:
             print(i + ' : ' + str(user_cart[i]))
         
     elif choice == 'buy': 
-        # logic for buy.
         print('what brand do you want to purchase: ')
-        choice = input()# example: Nike
+        choice = input()
         if choice in brands:
             print('Are you sure you want to purchase?: ')
-            yes_or_no = input()# example: yes or no
+            yes_or_no = input()
             if yes_or_no == 'yes':
                 print('purchase successful! ')
                 user_cart[choice] += 1
@@ -48,7 +37,6 @@ while True:
             print('brand not found')
 
     elif choice == 'sell':
-        #logic for sell.
         print('What brand do you want to sell?: ')
         choice = input()
         if choice in brands:
@@ -68,33 +56,3 @@ while True:
 
     elif choice == 'quit':
         quit()
-
-    
-
-
-
-   
-
-   
-
-
-
-
-
-
-
-
-
-# brands = 'Nike'
-# choice = input('what is a brand that you are seartching for:')
-
-# while choice != brands:
-#     choice = input('selection not found - try again. Find brand:') 
-
-# print('brand found')
-
-
- 
-
-
-
